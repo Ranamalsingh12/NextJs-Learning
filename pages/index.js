9fe2725077bacb5b3b5c9b33c116c2da4f7c7f3a
Ranamalsingh12 <ranams99911@gmail.com>
2022-11-19 21:21:18 +0530

    swr added in index.js

--- pages/index.js
+++ pages/index.js
@@ -53,3 +53,13 @@ export default function Home({ allPostsData }) {
     </Layout>
   );
 }
+
+// import useSWR from 'swr';
+
+// function Profile() {
+//   const { data, error } = useSWR('/api/user', fetch);
+
+//   if (error) return <div>failed to load</div>;
+//   if (!data) return <div>loading...</div>;
+//   return <div>hello {data.name}!</div>;
+// }
