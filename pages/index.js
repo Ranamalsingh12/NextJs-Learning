5a405d4dd874eb3fc567945cef73eeee7ba5c40e
Ranamalsingh12 <ranams99911@gmail.com>
2022-11-19 19:41:51 +0530

    using getStaticProps for Static side generation

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,54 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// export default function Home() {
+//   return (
+//     <Layout home>
+//       <Head>
+//         <title>{siteTitle}</title>
+//       </Head>
+//       <section className={utilStyles.headingMd}>
+//         <p>
+// I am Rana Mal Singh, Currently I am working on Web Development. I am a Front-end Developer with experience building projects. I am a tech & an open-source enthusiast. Currently i am learning Web3, Java, Next.js, Node.js, Backend Development and Data Structure & Algorithm.  </p>
+//         <p>
+//           (This is a sample website - you’ll be building a site like this on{' '}
+//           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+//         </p>
+//       </section>
+//     </Layout>
+//   );
+// }
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-I am Rana Mal Singh, Currently I am working on Web Development. I am a Front-end Developer with experience building projects. I am a tech & an open-source enthusiast. Currently i am learning Web3, Java, Next.js, Node.js, Backend Development and Data Structure & Algorithm.  </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
